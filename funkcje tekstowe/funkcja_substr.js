9289ad0954f0daae0eff3442ae814ee46f25cc77
Kaluza6 <144929244+Kaluza6@users.noreply.github.com>
2025-10-21 12:14:31 +0200

    drtyud

--- funkcje tekstowe/funkcja_substr.js
+++ funkcje tekstowe/funkcja_substr.js
@@ -4,13 +4,13 @@ let trzeci = document.getElementById("trzeci")
 let przyciskpierwszy = document.getElementById("przyciskpierwszy")
 let przyciskdrugi = document.getElementById("przyciskdrugi")
 przyciskpierwszy.addEventListener("click", function(){
-    drugi.innerText = pierwszy.innerText.slice(27, 36)
+    drugi.innerText = pierwszy.innerText.substr(27, 9)
     drugi.style.color = "yellow"
     drugi.style.backgroundColor = "dodgerblue"
     drugi.style.padding = "5px"
 })
 przyciskdrugi.addEventListener("click", function(){
-    trzeci.innerText = pierwszy.innerText.slice(37)
+    trzeci.innerText = pierwszy.innerText.substr(37)
     trzeci.style.color = "yellow"
     trzeci.style.backgroundColor = "tomato"
     trzeci.style.padding = "5px"
