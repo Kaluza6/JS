f396d289b7209ab02e048c5ad6ca11e94b5750b3
Kaluza6 <144929244+Kaluza6@users.noreply.github.com>
2025-11-12 11:10:31 +0100

    qwerty

---
+++ zegar/zegar.js
@@ -0,0 +1,21 @@
+let godz = document.getElementById('godz')
+let lgodz = document.getElementById('lgodz')
+let minuty = document.getElementById('minuty')
+let lmin = document.getElementById('lmin')
+let kat = document.getElementById('kat')
+Math.abs(30*godz.value-(11*minuty.value)/2)
+godz.value=0
+minuty.value=0
+kat.innerHTML = 0+'&deg;'
+lgodz.innerText=godz.value
+godz.addEventListener("input", function(){
+lgodz.innerText=godz.value
+kat.innerHTML = Math.abs(30*godz.value-(11*minuty.value)/2)+'&deg;'
+
+})
+lmin.innerText=minuty.value
+minuty.addEventListener("input", function(){
+lmin.innerText=minuty.value
+kat.innerHTML = Math.abs(30*godz.value-(11*minuty.value)/2)+'&deg;'
+})
+
